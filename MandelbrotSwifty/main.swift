a514de72f7e4a534dea3c3d526acbc3c567f2f39
Pavol Vaskovic <pali@pali.sk>
2017-01-30 09:51:09 +0100

    Initial Commit

---
+++ MandelbrotSwifty/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MandelbrotSwifty
+//
+//  Created by Pavol Vaskovic on 30.01.17.
+//  Copyright © 2017 Pavol Vaskovic. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

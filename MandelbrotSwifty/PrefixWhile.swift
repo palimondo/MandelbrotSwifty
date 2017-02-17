827b60f2289667218d3fd4d960110d905d60d704
Pavol Vaskovic <pali@pali.sk>
2017-02-17 09:56:12 +0100

    Err… moving files around.

--- MandelbrotSwifty/PrefixWhile.swift
+++ MandelbrotSwifty/PrefixWhile.swift
@@ -15,31 +15,31 @@ extension Sequence {
     public typealias Predicate = (Self.Iterator.Element) -> Bool
     public typealias PredicatedIterator = (predicate: Predicate, iterator: Self.Iterator)
     
-    public func __prefix (while predicate: @escaping Predicate) ->
-        UnfoldSequence<Self.Iterator.Element, PredicatedIterator> {
-            func nextMatch(state: inout PredicatedIterator) -> Iterator.Element? {
-                guard
-                    let e = state.iterator.next()
-                    else { return nil }
-                return state.predicate(e) ? e : nil
-            }
-            return sequence(state: (predicate, makeIterator()), next: nextMatch)
-    }
-    
-    internal func nextMatch(state: inout PredicatedIterator) -> Iterator.Element? {
-        guard
-            let e = state.iterator.next()
-            else { return nil }
-        return state.predicate(e) ? e : nil
-    }
-    
-    
-    public func ___prefix (while predicate: @escaping Predicate) ->
-        UnfoldSequence<Self.Iterator.Element, PredicatedIterator> {
-            return sequence(state: (predicate, makeIterator()), next: nextMatch)
-    }
+//    public func __prefix (while predicate: @escaping Predicate) ->
+//        UnfoldSequence<Self.Iterator.Element, PredicatedIterator> {
+//            func nextMatch(state: inout PredicatedIterator) -> Iterator.Element? {
+//                guard
+//                    let e = state.iterator.next()
+//                    else { return nil }
+//                return state.predicate(e) ? e : nil
+//            }
+//            return sequence(state: (predicate, makeIterator()), next: nextMatch)
+//    }
+//    
+//    internal func nextMatch(state: inout PredicatedIterator) -> Iterator.Element? {
+//        guard
+//            let e = state.iterator.next()
+//            else { return nil }
+//        return state.predicate(e) ? e : nil
+//    }
+//    
+//    
+//    public func ___prefix (while predicate: @escaping Predicate) ->
+//        UnfoldSequence<Self.Iterator.Element, PredicatedIterator> {
+//            return sequence(state: (predicate, makeIterator()), next: nextMatch)
+//    }
 }
 
 let pw = Array(ys._prefix(while: {$0 > 2}))
-let ppw = Array(ys.__prefix(while: {$0 > 2}))
-let pppw = Array(ys.___prefix(while: {$0 > 2}))
+//let ppw = Array(ys.__prefix(while: {$0 > 2}))
+//let pppw = Array(ys.___prefix(while: {$0 > 2}))
